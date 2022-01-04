f6111d2186dd7d2b5d78552fd9886fa705182d7d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-01-04 10:58:07 +0900

    feat : 커스텀 구분자에 대한 검증

--- StringCalculator/src/main/java/StringCalculatorService.java
+++ StringCalculator/src/main/java/StringCalculatorService.java
@@ -1,9 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.regex.Pattern;
 
 public class StringCalculatorService {
 
-	private static final String COMMA = ",";
-	private static final String COLON = ":";
+	private static final int CUSTOM_SEPARATOR_INDEX = 2;
+	private static final int NUMBER_START_INDEX = 5;
 
 	// public int calculateString(String userString) {
 	//
@@ -12,5 +13,27 @@ public class StringCalculatorService {
 	public boolean isDefaultSeparator(String userString) {
 		String defaultRegex = "(\\d*[,:])*\\d*";
 		return Pattern.matches(defaultRegex, userString);
+	}
+
+	public boolean isCustomSeparator(String userString) {
+		String customRegex = "\\/\\/(.)\\\\n([\\d]+.)*[\\d]+";
+		if (!Pattern.matches(customRegex, userString)) {
+			return false;
+		}
+		return isValidString(userString);
+	}
+
+	private boolean isValidString(String userString) {
+		String customSeparator = userString.split("")[CUSTOM_SEPARATOR_INDEX];
+		String[] numberString = userString.substring(NUMBER_START_INDEX)
+			.split(customSeparator);
+		for (String s : numberString) {
+			try {
+				Integer.parseInt(s);
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- StringCalculator/src/test/java/StringCalculatorTest.java
+++ StringCalculator/src/test/java/StringCalculatorTest.java
@@ -1,13 +1,19 @@
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
+	private static StringCalculatorService sd;
+
+	@BeforeAll
+	public static void setSd() {
+		sd = new StringCalculatorService();
+	}
 
 	@Test
 	void 기본_구분자_문자열() {
 		//given
-		StringCalculatorService sd = new StringCalculatorService();
 
 		//when
 		String userString = "113,22:1173";
@@ -16,6 +22,30 @@ public class StringCalculatorTest {
 		//then
 		assertThat(sd.isDefaultSeparator(userString)).isTrue();
 		assertThat(sd.isDefaultSeparator(exceptionString)).isFalse();
+	}
+
+	@Test
+	void 커스텀_구분자_문자열() {
+		//given
+
+		//when
+		String userString = "//<\\n1<2<3";
+		String exceptionString = "//`\\n1a`2`3";
+
+		//then
+		assertThat(sd.isCustomSeparator(userString)).isTrue();
+		assertThat(sd.isCustomSeparator(exceptionString)).isFalse();
+	}
+
+	@Test
+	void 커스텀_구분자() {
+		//given
+
+		//when
+		String exceptionString = "//<\\n1:2:3`";
+
+		//then
+		assertThat(sd.isCustomSeparator(exceptionString)).isFalse();
 	}
 
 	// @Test
