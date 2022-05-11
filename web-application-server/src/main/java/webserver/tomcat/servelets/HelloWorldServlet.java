5fd95a6db98be02125445a3268eed6a269fc202f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-05-11 23:54:10 +0900

    chore : move README.md

--- web-application-server/src/main/java/webserver/tomcat/servelets/HelloWorldServlet.java
+++ web-application-server/src/main/java/webserver/tomcat/servelets/HelloWorldServlet.java
@@ -3,15 +3,16 @@ package webserver.tomcat.servelets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(value = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello World!");
 	}
